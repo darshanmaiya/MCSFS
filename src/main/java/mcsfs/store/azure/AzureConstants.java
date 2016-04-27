9ec9df6346b933008402f3cb405f38bb2987ceaa
Wei-Tsung Lin <fasthall@gmail.com>
2016-04-27 16:12:53 -0700

    change azure working directory

--- src/main/java/mcsfs/store/azure/AzureConstants.java
+++ src/main/java/mcsfs/store/azure/AzureConstants.java
@@ -1,10 +1,12 @@
 package mcsfs.store.azure;
 
+import mcsfs.Constants;
+
 public class AzureConstants {
 
 	public static final String AZURE_ACCOUNT_NAME = "";
 	public static final String AZURE_ACCOUNT_KEY = "";
 	public static final String AZURE_CONTAINER_NAME = "mcsfs";
-	public static final String AZURE_DIRECTORY = "azure";
+	public static final String AZURE_DIRECTORY = Constants.MCSFS_WORKING_DIR ;
 
 }
